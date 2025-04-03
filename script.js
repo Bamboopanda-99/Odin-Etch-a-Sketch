f50a22001902d7ebd551722f471e0a7b6f2d7200
BambooPanda <123456789+odin@users.noreply.github.com>
2025-04-03 13:32:15 -0700

    added more style and finished the project

---
+++ script.js
@@ -0,0 +1,38 @@
+const container = document.getElementById("container");
+const btns = Array.from(document.querySelectorAll(".color-selector"));
+let bgColor = "yellow";
+
+createGrid(16);
+
+function createGrid(num) {
+  for (let i = 1; i <= num; i++) {
+    let column = document.createElement("div");
+    column.classList.add("column");
+    container.appendChild(column);
+    for (let j = 1; j <= num; j++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      column.appendChild(row);
+      row.addEventListener("mouseover", function (e) {
+        e.target.style.backgroundColor = bgColor;
+      });
+    }
+  }
+}
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    bgColor = btn.value;
+  });
+});
+
+function createNew() {
+  let userInput = prompt("Please input a number between 2 and 100");
+  if (userInput < 2 || userInput > 100) {
+    alert("Please enter a valid number");
+    createNew();
+  } else {
+    container.innerHTML = "";
+    createGrid(userInput);
+  }
+}
